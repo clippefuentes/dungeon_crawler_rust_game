a04aae4f13f32d0e037eda0e7ce7285d78f245b3
Clyne Fuentes <clippefuentes@gmail.com>
2022-10-25 18:57:05 -0600

    add entities

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 #![warn(clippy::pedantic)]
 
-mod map;
-mod map_builder;
 mod camera;
 mod components;
+mod map;
+mod map_builder;
 mod spawner;
+mod systems;
 
-mod prelude { 
-    pub use bracket_lib::prelude::*; 
-    pub use legion::*;
-    pub use legion::world::SubWorld;
+mod prelude {
+    pub use bracket_lib::prelude::*;
     pub use legion::systems::CommandBuffer;
-    pub const SCREEN_WIDTH: i32 = 80; 
+    pub use legion::world::SubWorld;
+    pub use legion::*;
+    pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-    pub use crate::map::*; 
-    pub use crate::map_builder::*;
     pub use crate::camera::*;
     pub use crate::components::*;
-    pub use crate::spawner::*; 
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
 }
 
-use prelude::*; 
+use prelude::*;
 
 struct State {
     ecs: World,
@@ -37,12 +39,18 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         Self {
             ecs,
             resources,
-            systems: build_scheduler()
+            systems: build_scheduler(),
         }
     }
 }
@@ -53,6 +61,9 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
+        self.resources.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 
@@ -60,13 +71,13 @@ fn main() -> BError {
     let context = BTermBuilder::new()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
-        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) 
-        .with_tile_dimensions(32, 32) 
-        .with_resource_path("resources/") 
-        .with_font("dungeonfont.png", 32, 32) 
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") 
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT,  "dungeonfont.png")
-        .build()?; 
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .build()?;
 
     main_loop(context, State::new())
 }
---
+++ src/mod.rs
@@ -0,0 +1,7 @@
+use crate::prelude::*;
+
+pub fn build_scheduler() -> Schedule {
+    Schedule::builder()
+        .add_system(player_input::player_input_system())
+        .build()
+}
