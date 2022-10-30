77e9d302eca055ed51aa5d70846f38d315bea277
Clyne Fuentes <clippefuentes@gmail.com>
2022-10-30 16:57:32 -0600

    add enemies to move with pattern instead of moving random

--- src/spawner.rs
+++ src/spawner.rs
@@ -9,8 +9,8 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             glyph: to_cp437('@'),
         },
         Health {
-            current: 20,
-            max: 20,
+            current: 10,
+            max: 10,
         },
     ));
 }
@@ -26,16 +26,14 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
         pos,
         Render {
             color: ColorPair::new(WHITE, BLACK),
-            glyph: match rng.range(0, 4) {
-                0 => to_cp437('E'),
-                1 => to_cp437('O'),
-                2 => to_cp437('o'),
-                _ => to_cp437('g'),
-            },
+            glyph,
         },
-        MovingRandomly {},
-        Health{current: hp, max: hp},
-        Name(name)
+        ChasingPlayer {},
+        Health {
+            current: hp,
+            max: hp,
+        },
+        Name(name),
     ));
 }
 
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,15 +1,15 @@
 use crate::prelude::*;
 
-mod map_render;
+mod chasing;
+mod combat;
+mod end_turn;
 mod entity_render;
+mod hud;
+mod map_render;
+mod movement;
 mod player_input;
 mod random_move;
-mod end_turn;
-mod movement;
-mod hud;
 mod tooltips;
-mod combat;
-
 
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
@@ -38,6 +38,7 @@ pub fn build_player_scheduler() -> Schedule {
 pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
+        .add_system(chasing::chasing_system())
         .flush()
         .add_system(combat::combat_system())
         .flush()
@@ -58,5 +59,5 @@ pub fn build_monster_scheduler() -> Schedule {
 //         .add_system(map_render::map_render_system())
 //         .add_system(entity_render::entity_render_system())
 //         .add_system(random_move::random_move_system())
-//         .build() 
+//         .build()
 // }
