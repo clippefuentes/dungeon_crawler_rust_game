2795fff3325e62c8dc368f9e4aebb62afb35ad19
Clyne Fuentes <clippefuentes@gmail.com>
2022-11-05 15:11:05 -0600

    add ways to generate maps

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,37 +1,62 @@
 use crate::prelude::*;
 
-const NUM_ROOMS: usize = 2;
+mod empty;
+// use empty::EmptyArchitect;
+
+mod rooms;
+use rooms::RoomsArchitect;
+
+mod automata;
+use automata::CellularAutomataArchitect;
+
+mod drunkard;
+use drunkard::DrunkardsWalkArchitect;
+
+// NUM OF ROOMS TEST
+const NUM_ROOMS: usize = 5;
+
+trait MapArchitect {
+    fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
+}
 
 pub struct MapBuilder {
     pub map: Map,
     pub rooms: Vec<Rect>,
+    pub monster_spawns: Vec<Point>,
     pub player_start: Point,
     pub amulet_start: Point,
 }
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut mb = MapBuilder {
-            map: Map::new(),
-            rooms: Vec::new(),
-            player_start: Point::zero(),
-            amulet_start: Point::zero(),
-        };
-        mb.fill(TileType::Wall);
-        mb.build_random_rooms(rng);
-        mb.build_corridors(rng);
-        mb.player_start = mb.rooms[0].center();
+        // let mut architect = CellularAutomataArchitect {};
+        // architect.new(rng)
+        let mut architect = DrunkardsWalkArchitect{};
+        architect.new(rng)
+    }
 
+    // pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+    //     let mut architect : Box<dyn MapArchitect> = match rng.range(0, 3) {
+    //         0 => Box::new(DrunkardsWalkArchitect{}),
+    //         1 => Box::new(RoomsArchitect{}),
+    //         _ => Box::new(CellularAutomataArchitect{})
+    //    };
+    //    let mut mb = architect.new(rng); 
+    //    mb
+    // }
+     
+
+    fn find_most_distant(&self) -> Point {
         let dijkstra_map = DijkstraMap::new(
             SCREEN_WIDTH,
             SCREEN_HEIGHT,
-            &vec![mb.map.point2d_to_index(mb.player_start)],
-            &mb.map,
+            &vec![self.map.point2d_to_index(self.player_start)],
+            &self.map,
             1024.0,
         );
 
         const UNREACHABLE: &f32 = &f32::MAX;
-        mb.amulet_start = mb.map.index_to_point2d(
+        self.map.index_to_point2d(
             dijkstra_map
                 .map
                 .iter()
@@ -40,9 +65,7 @@ impl MapBuilder {
                 .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
                 .unwrap()
                 .0,
-        );
-
-        mb
+        )
     }
 
     fn fill(&mut self, tile: TileType) {
@@ -110,5 +133,29 @@ impl MapBuilder {
                 self.apply_horizontal_tunnel(prev.x, new.x, new.y);
             }
         }
+    }
+
+    fn spawn_monsters(&self, start: &Point, rng: &mut RandomNumberGenerator) -> Vec<Point> {
+        const NUM_MONSTERS: usize = 50;
+        let mut spawnable_tiles: Vec<Point> = self
+            .map
+            .tiles
+            .iter()
+            .enumerate()
+            .filter(|(idx, t)| {
+                **t == TileType::Floor
+                    && DistanceAlg::Pythagoras.distance2d(*start, self.map.index_to_point2d(*idx))
+                        > 10.0
+            })
+            .map(|(idx, _)| self.map.index_to_point2d(idx))
+            .collect();
+
+        let mut spawns = Vec::new();
+        for _ in 0..NUM_MONSTERS {
+            let target_index = rng.random_slice_index(&spawnable_tiles).unwrap();
+            spawns.push(spawnable_tiles[target_index].clone());
+            spawnable_tiles.remove(target_index);
+        }
+        spawns
     }
 }
--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -14,6 +14,7 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
         for x in camera.left_x..camera.right_x {
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
+            let idx = map_idx(x, y);
             // if map.in_bounds(pt) {
             //     let idx = map_idx(x, y);
             //     let glyph = match map.tiles[idx] {
@@ -22,14 +23,19 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
             //     };
             //     draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), glyph);
             // }
-            if map.in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
+            if map.in_bounds(pt) && (player_fov.visible_tiles.contains(&pt) | map.revealed_tiles[idx]) {
+                let tint = if player_fov.visible_tiles.contains(&pt) {
+                    WHITE
+                } else {
+                    DARK_GRAY
+                };
                 let idx = map_idx(x, y);
                 match map.tiles[idx] {
                     TileType::Floor => {
-                        draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), to_cp437('.'));
+                        draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), to_cp437('.'));
                     }
                     TileType::Wall => {
-                        draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), to_cp437('#'));
+                        draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), to_cp437('#'));
                     }
                 }
             }
